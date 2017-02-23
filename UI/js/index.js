207c1f7bb5b7292871f3a22819af69b3c93cddc2
Risham Chokshi <rychoksh@us.ibm.com>
2017-02-22 21:56:54 -0500

    added search.html and all the main page

--- UI/js/index.js
+++ UI/js/index.js
@@ -41,3 +41,35 @@ $('.tab a').on('click', function (e) {
   $(target).fadeIn(600);
   
 });
+
+$('.open_search').on('click',function (e){
+  document.location.href = "./search.html";
+  //console.log(this.parentNode.parentNode.id === 'signup');
+  /*var goToSearchPage = true;
+  if(this.parentNode.parentNode.id === 'signup'){
+    $('#signup').find('div.field-wrap>input').each(function(){
+      /*console.log($(this).text() === '');
+      console.log($(this).hasClass("check_req"));
+      if($(this).text() === '' && $(this).hasClass("check_req")){
+        goToSearchPage = false;
+      }
+      //try to add this field in there    
+    });
+    if(goToSearchPage)
+      document.location.href = "./search.html";
+  }
+   else {
+    if(this.parentNode.parentNode.id === 'login'){
+    $('#login').find('div.field-wrap>input').each(function(){
+      if($(this).text() === '' && $(this).hasClass("check_req")){
+        goToSearchPage = false;
+        console.log(false);
+      }
+      //try to add this field in there    
+    });
+    console.log("all good");
+    //if(goToSearchPage)
+      //document.location.href = "./search.html";
+  }
+}*/
+});
