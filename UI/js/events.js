b8d938faa31db7bd5b3b7a9ba05e89fdf88ba2c7
Risham Chokshi <rychoksh@us.ibm.com>
2017-04-01 02:00:04 -0400

    added jquery for search_tab

---
+++ UI/js/events.js
@@ -0,0 +1,36 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    /*
+		This is function for whatever is entered in the text box,
+		it will spin out information about that location
+    */
+    $('#search > a').on("click",function(){
+    	//check for the empty value
+    	if($("#search > input").val() == ""){
+    		alert("Please put a value in input box");
+    	} else {
+    		$("#portfolio h4.section-subheading").text($("#search > input").val());
+    		$("#event_tab").css("display","block");
+    		$("#portfolio").css("display","block");
+    		$('#search > a').click();
+    		//click on the function below, so you can get the information
+    		//loading
+
+    	}	
+    });
+
+    /*
+		Once an event is clicked, all the friends would be populated
+    */
+    var event_friends = function(location){
+
+    }
+
+    /*
+		Once an event is clicked, all the other events would be populated
+    */
+    var other_event = function(location){
+    	
+    }
+})(jQuery); // End of use strict
