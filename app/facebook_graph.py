450cd7153ac1d828264f0a395cca702c2b66b908
Saurabh Deochake <saurabhd04@localhost.localdomain>
2017-04-20 02:55:48 -0400

    prints the IDs of all people attending an event

--- app/facebook_graph.py
+++ app/facebook_graph.py
@@ -18,7 +18,9 @@ from ConfigParser import SafeConfigParser
 
 CONFIG_FILE = "/etc/api_config"
 EVENT_LIMIT = 10000
-
+ATTENDING_LIMIT = 100000000
+LOCATION = "Prudential Center"
+SELF_ID = "10203154386536494"
 
 class FacebookGraph:
 
@@ -37,10 +39,18 @@ class FacebookGraph:
 	def get_current_time(self):
 		return datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
-	def get_event_ids(self, ids, info):
+	def get_event_json(self, info):
 		for i in info["data"]:
-			ids.append(i["id"])
-		return set(ids)
+			if "end_time" in i and "place" in i and "name" in i["place"] and "start_time" in i and "description" in i:
+				d = {"id": i["id"], "name": i["name"], "description": i["description"],"start_time": i["start_time"], "end_time":i["end_time"], 
+		      	     "location":i["place"]["name"]}
+			else:
+				continue
+			events_info.append(d)
+	
+		events = {"data": events_info}
+		return json.dumps(events)
+	
 
 	#def __unicode__(self):
 	#	return unicode(self.some_field) or u''
@@ -54,27 +64,33 @@ if __name__ == '__main__':
 	ids = []
 	events = {}
 	events_info = []
-	data =fb_call.request("search",{ 'q' : 'Chicago', 'type' : 'event', 'limit':EVENT_LIMIT, 'since_date'  :   'currentTime'}) #'limit' ,before since_date
+	data =fb_call.request("search",{ 'q' : LOCATION, 'type' : 'event', 'limit':EVENT_LIMIT, 'since_date'  :   'currentTime'}) #'limit' ,before since_date
 	
 	# fb.get_data already returns us output in json format
 	info = json.loads(fb.get_data(data))
 	
+	events = json.loads(fb.get_event_json(info))
 	#print len(fb.get_event_ids(ids, info))
 
-	for i in info["data"]:
-		if "end_time" in i and "place" in i and "name" in i["place"] and "start_time" in i and "description" in i:
-			d = {"id": i["id"], "name": i["name"], "description": i["description"],"start_time": i["start_time"], "end_time":i["end_time"], 
-		      	"location":i["place"]["name"]}
+	#print events
+	# ------------_ DO IT FOR ALL IDS _------------------
+	id = events["data"][0]["id"]
 
-		else:
-			d = {"id": i["id"], "name": i["name"], "start_time": i["start_time"]}
+	#print fb_call.get_object(id, fields=[])
+	#print id
+	connections = fb_call.get_connections(id, "attending",limit=ATTENDING_LIMIT,fields=[])
+	#print connections
 
-		events_info.append(d)
-	
-	events = {"data": events_info}
-	events = json.dumps(events)
-	print events
-	
+	for name in connections["data"]:
+		print name["id"]
+		#pass
+
+	#print fb_call.get_object(id="me", fields=[])
+	#id = 10203154386536494
+	#user = fb_call.get_object("me")
+	#print user
+	#print fb_call.get_connections(user["id"],"friends",id)
+	#print me["data"]
 	# Now we have to get 
 	# 1. All Event IDs
 	# 2. Start and End dates
